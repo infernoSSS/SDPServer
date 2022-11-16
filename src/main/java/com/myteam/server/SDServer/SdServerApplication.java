876ddbdded299b0cc348045ea6651b29b00b7878
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2022-11-16 23:00:24 +0300

    test

--- src/main/java/com/myteam/server/SDServer/SdServerApplication.java
+++ src/main/java/com/myteam/server/SDServer/SdServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdServerApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(SdServerApplication.class, args);
 	}
