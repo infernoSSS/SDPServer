2f5fca89e87bb2371d5a011fd0604f25a897f9fc
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2022-11-16 22:49:19 +0300

    Initial commit

---
+++ src/main/java/com/myteam/server/SDServer/SdServerApplication.java
@@ -0,0 +1,13 @@
+package com.myteam.server.SDServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myteam/server/SDServer/SdServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteam.server.SDServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
